555980aed01e9853c43d91fa00b7fadfa80bdbae
umar <umar110302@gmail>
2024-08-19 06:17:24 +0100

    views.py and blueprints

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
-    return render_template('base.html')
+from main.views import main_blueprint
+from blog.views import blog_blueprint
+
+app.register_blueprint(main_blueprint)
+app.register_blueprint(blog_blueprint)
+
+
 
 if __name__ == '__main__':
     app.run()
